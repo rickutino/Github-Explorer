d5d989ad5a77531be600e1bfd9804e02671edbd5
rickutino <rickutino@gmail.com>
2020-09-13 17:12:56 +0900

    検索結果一覧表作成

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import logoImg from '../../assets/logo.svg';
-import { Title, Form } from './styles';
+import logoImg from '../../assets/logo.png';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -13,6 +14,75 @@ const Dashboard: React.FC = () => {
         <input placeholder="Type name of the repository" />
         <button type="submit">Search</button>
       </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://pbs.twimg.com/profile_images/1300177327221927936/ksGbaONc_400x400.jpg"
+            alt="Ricardo Utino"
+          />
+          <div>
+            <strong>SpotCode</strong>
+            <p>Spotify copy site.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://pbs.twimg.com/profile_images/1300177327221927936/ksGbaONc_400x400.jpg"
+            alt="Ricardo Utino"
+          />
+          <div>
+            <strong>SpotCode</strong>
+            <p>Spotify copy site.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://pbs.twimg.com/profile_images/1300177327221927936/ksGbaONc_400x400.jpg"
+            alt="Ricardo Utino"
+          />
+          <div>
+            <strong>SpotCode</strong>
+            <p>Spotify copy site.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://pbs.twimg.com/profile_images/1300177327221927936/ksGbaONc_400x400.jpg"
+            alt="Ricardo Utino"
+          />
+          <div>
+            <strong>SpotCode</strong>
+            <p>Spotify copy site.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://pbs.twimg.com/profile_images/1300177327221927936/ksGbaONc_400x400.jpg"
+            alt="Ricardo Utino"
+          />
+          <div>
+            <strong>SpotCode</strong>
+            <p>Spotify copy site.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="test">
+          <img
+            src="https://pbs.twimg.com/profile_images/1300177327221927936/ksGbaONc_400x400.jpg"
+            alt="Ricardo Utino"
+          />
+          <div>
+            <strong>SpotCode</strong>
+            <p>Spotify copy site.</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
